e580671e57eedd95e1a3550a3210e9723c9f3ad8
merzillin <spartacusraphealpaul@gmail.com>
2026-02-28 20:40:01 +0530

    router updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'  
-import './index.css'
+import { HashRouter } from 'react-router-dom'   
 import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/Project-Rico">  
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+    <HashRouter>
+      <App />  
+    </HashRouter>
+  </StrictMode>
 )
